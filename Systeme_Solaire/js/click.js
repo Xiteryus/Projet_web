f6016c9a803d8679cd660ac42d7af62b31cfcbea
david <david.watine@gmail.com>
2024-05-25 21:16:43 +0200

    mars et css

--- Systeme_Solaire/js/click.js
+++ Systeme_Solaire/js/click.js
@@ -2,10 +2,21 @@ function f(event) {
     var x = event.offsetX;
     var y = event.offsetY;
 
-    var pointX = 609;
-    var pointY = 380;
 
-    if (x >= pointX - 20 && x <= pointX + 20 && y >= pointY - 20 && y <= pointY + 20) {
+
+    if (x >= 609 - 20 && x <= 609 + 20 && y >= 380 - 20 && y <= 380 + 20) {
         window.location.href = "Mercure.html";
+    }
+
+    if (x >= 720 - 20 && x <= 720 + 20 && y >= 380 - 20 && y <= 380 + 20) {
+        window.location.href = "Venus.html";
+    }
+
+    if (x >= 900 - 20 && x <= 900 + 20 && y >= 380 - 20 && y <= 380 + 20) {
+        window.location.href = "Terre.html";
+    }
+
+    if (x >= 1050 - 23 && x <= 1050  && y >= 380 - 10 && y <= 380 + 10) {
+        window.location.href = "mars.html";
     }
 }
