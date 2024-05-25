a8113c3a442f0244f22e264c2e3afc32ff33bb90
david <david.watine@gmail.com>
2024-05-25 17:46:46 +0200

    Co-authored-by: Xiteryus <Xiteryus@users.noreply.github.com>

--- Systeme_Solaire/Systeme_Solaire/js/click.js
+++ Systeme_Solaire/Systeme_Solaire/js/click.js
@@ -4,7 +4,8 @@ function f(event) {
 
 
 
-    if (x >= 609 - 20 && x <= 609 + 20 && y >= 380 - 20 && y <= 380 + 20) {
+    if (x >= 609 - 25 && x <= 609 - 10 && y >= 380 - 10 && y <= 380 + 10) {
+        
         window.location.href = "Mercure.html";
     }
 
@@ -12,7 +13,11 @@ function f(event) {
         window.location.href = "Venus.html";
     }
 
-    if (x >= 900 - 20 && x <= 900 + 20 && y >= 380 - 20 && y <= 380 + 20) {
+    if (x >= 900 - 35 && x <= 900 + 5 && y >= 380 - 20 && y <= 380 + 20) {
         window.location.href = "Terre.html";
+    }
+
+    if (x >= 1050 - 23 && x <= 1050  && y >= 380 - 10 && y <= 380 + 10) {
+        window.location.href = "mars.html";
     }
 }
