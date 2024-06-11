1750c8ca841444dce3e639fbea6e6b5c4d3dcec5
samue <xiteryus@gmail.com>
2024-06-11 16:52:05 +0200

    Soleil + Mercure + Venus + Terre + Mars + Ceres = Ok

--- Systeme_Solaire/js/click.js
+++ Systeme_Solaire/js/click.js
@@ -93,9 +93,60 @@ function survole(event, page) {
         }
     }
 
-    else if(page == "mercure"){}
-    else if(page == "venus"){}
-    else if(page == "terre"){}
+    else if(page == "Mercure"){
+        if (x >= 42 - 5 && x <= 42 + 5 && y >= 413 - 5 && y <= 413 + 5) {
+            afficherp(p, x, y, "Basho",30,50);
+        }
+        else if (x >= 1096 - 5 && x <= 1096 + 5 && y >= 338 - 5 && y <= 338 + 5) {
+            afficherp(p, x, y, "Eminescu",30,50);
+        }
+        else if (x >= 654 - 20 && x <= 654 + 20 && y >= 353 - 20 && y <= 353 + 20) {
+            afficherp(p, x, y, "Debussy",30,50);
+        }
+        else if (x >= 381 - 5 && x <= 381 + 5 && y >= 265 - 5 && y <= 265 + 5) {
+            afficherp(p, x, y, "Lermontov",30,50);
+        }
+        else if (x >= 391 - 5 && x <= 391 + 5 && y >= 387 - 5 && y <= 387 + 5) {
+            afficherp(p, x, y, "Murasaki",30,50);
+        }
+        else {
+            p.style.display = "none";
+        }
+    }
+    else if(page == "venus"){
+        if (x >= 586 - 15 && x <= 586 + 15 && y >= 515 - 15 && y <= 515 + 15) {
+            afficherp(p, x, y, "Maxwell montes",30,50);
+        }
+        else if (x >= 738 - 5 && x <= 738 + 5 && y >= 361 - 5 && y <= 361 + 5) {
+            afficherp(p, x, y, "Var Mons",30,50);
+        }
+        else if (x >= 723 - 20 && x <= 723 + 20 && y >= 386 - 20 && y <= 386 + 20) {
+            afficherp(p, x, y, "Venera 14",30,50);
+        }
+        else if (x >= 288 - 15 && x <= 288 + 15 && y >= 281 - 15 && y <= 281 + 15) {
+            afficherp(p, x, y, "Ovda Regio",30,50);
+        }
+        else if (x >= 174 - 15 && x <= 174 + 15 && y >= 273 - 15 && y <= 273 + 15) {
+            afficherp(p, x, y, "Thetis Regio",30,50);
+        }
+        else {
+            p.style.display = "none";
+        }
+    }
+    else if(page == "terre"){
+        if (x >= 626 - 15 && x <= 626 + 15 && y >= 146 - 15 && y <= 146 + 15) {
+            afficherp(p, x, y, "Alpes - Mont Blanc",30,50);
+        }
+        else if (x >= 846 - 5 && x <= 846 + 5 && y >= 180 - 5 && y <= 180 + 5) {
+            afficherp(p, x, y, "Himalaya - Everest ",30,50);
+        }
+        else if (x >= 1062 - 20 && x <= 1062 + 20 && y >= 264 - 20 && y <= 264 + 20) {
+            afficherp(p, x, y, "Fosse des Mariannes",30,50);
+        }
+        else {
+            p.style.display = "none";
+        }
+    }
 
     else if(page == "mars"){
         if (x >= 154 - 5 && x <= 154 + 5 && y >= 233 - 5 && y <= 233 + 5) {
@@ -119,7 +170,7 @@ function survole(event, page) {
     }
     else if(page == "jupiter"){
         if (x >= 434 -20 && x <= 434+20 && y >= 364-20 && y <= 364+20) {
-            afficherp(p, x, y, "<b>Tache rouge junior</b>",30,50);
+            afficherp(p, x, y, "<b>Grande Tache rouge</b>",30,50);
         } 
         else {
             p.style.display = "none";
@@ -153,19 +204,49 @@ function closecaroussel() {
 
 
 //-----------------------------
-const imagesJupiter = [
-    {src: '../img/Jupiter1.webp', legende: 'Nuage Jupiter' },
-    {src: '../img/jupiter2.webp', legende: 'Nuage Jupiter' },
-    {src: '../img/jupiter3.webp', legende: 'Nuage Jupiter' },
-    {src:'../img/jupiter4.jpg', legende :'Satelite de Jupiter : Europe'},
-    {src:'../img/Jupiter5.webp', legende: 'Anneaux de Jupiter vus par Voyager 2'}
-];
+
 const imagesSoleil = [
     {src: '../img/Soleil1.jpeg', legende: 'Soleil qui sourit' },
     {src: '../img/Soleil2.jpg', legende: 'Eruption solaire' },
     {src: '../img/Soleil3.jpg', legende: 'Le Soleil vu par le Solar Orbiter' },
     {src:'../img/Soleil4.webp', legende :'Les couches du Soleil'},
     {src:'../img/Soleil5.jpg', legende: 'Photo du Soleil avec 90000 images'}
+];
+const imagesMercure =[
+    {src:'../img/Mercure1.jpeg',legende:'Mercure'},
+    {src:'../img/Mercure2.jpeg',legende:'Mercure'},
+    {src:'../img/Mercure3.png',legende:'Les couches de Mercure'},
+];
+const imagesVenus =[
+    {src:'../img/Venus1.jpg',legende:'Surface de Vénus est cachée par son épaisse atmosphère'},
+    {src:'../img/Venus2.jpeg',legende:'Venus'},
+    {src:'../img/Venus3.jpg',legende:'Vue reconstituée du volcan vénusien Maat Mons à partir des images radar prises par la sonde Magellan en 1996.'},
+    {src:'../img/Venus4.webp',legende:'Photo colorisée de la surface de Vénus. Elle a été prise par le vaisseau spatial russe Venera 13 le 1er mars 1982.'},
+    {src:'../img/Venus5.jpg',legende:'Les couches de Ve,us'}];
+const imagesTerre =[
+    {src:'../img/Terre1.jpeg',legende:"Terre depuis l'espace"},
+    {src:'../img/Terre2.webp',legende:'Terre vu depuis lune'},
+    {src:'../img/Terre3.jpg',legende:"Terre vu depuis l'ISS"},
+    {src:'../img/Terre4.png',legende:'Les couches de la Terre'}
+];
+const imagesMars =[
+    {src:'../img/Mars1.jpeg',legende:'Mars'},
+    {src:'../img/Mars2.jpeg',legende:'Surface de Mars'},
+    {src:'../img/Mars3.jpeg',legende:'Surface de Mars et le rover MSL Curiosity'},
+    {src:'../img/Mars4.jpeg',legende:'Lune de Mars : Phobos'},
+    {src:'../img/Mars5.jpg',legende:'Les couches de Mars'}
+];
+const imagesCeres =[
+    {src:'../img/Ceres1.jpg',legende:'Ceres'},
+    {src:'../img/Ceres2.jpeg',legende:'Les couches de Ceres'},
+
+];
+const imagesJupiter = [
+    {src: '../img/Jupiter1.webp', legende: 'Nuage Jupiter' },
+    {src: '../img/jupiter2.webp', legende: 'Nuage Jupiter' },
+    {src: '../img/jupiter3.webp', legende: 'Nuage Jupiter' },
+    {src:'../img/jupiter4.jpg', legende :'Satelite de Jupiter : Europe'},
+    {src:'../img/Jupiter5.webp', legende: 'Anneaux de Jupiter vus par Voyager 2'}
 ];
 const imagesSaturne = [
     {src: '../img/saturne1.jpg', legende: 'Photo de Saturne' },
@@ -181,6 +262,9 @@ const imagesUranus = [
     {src:'../img/uranus4.jpg', legende :"Lune Ariel d'Uranus"},
     {src:'../img/uranus5.jpg', legende: 'Uranus pris par Hubble'}
 ];
+
+//-----------------------------
+
 let imagesPage;
 let currentIndex = 0;
 
@@ -189,6 +273,23 @@ function selectImageSet() {
     if (page === 'soleil') {
         imagesPage = imagesSoleil;
     } 
+    else if (page === 'Mercure') {
+        imagesPage = imagesMercure;
+    }
+    else if (page === 'Venus') {
+        imagesPage = imagesVenus;
+    }
+    else if (page === 'Terre') {
+        imagesPage = imagesTerre;
+    }
+    else if (page === 'mars') {
+        imagesPage = imagesMars;
+    }
+    else if (page === 'ceres') {
+        imagesPage = imagesCeres;
+    }
+
+
     else if (page === 'jupiter') {
         imagesPage = imagesJupiter;
     }
@@ -198,6 +299,7 @@ function selectImageSet() {
     else if (page === 'uranus') {
         imagesPage = imagesUranus;
     }
+
 }
 
 function updateCaroussel() {
